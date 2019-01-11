56ad1663ddb9d80e086226e7de92fa91a73e1171
Chase Terry <chaset@ChaseMM.local>
2019-01-11 11:08:13 -0600

    Moved navigation into its own file

--- application/views/navigation.php
+++ application/views/navigation.php
@@ -1,49 +1,32 @@
-<?php
-	$active=array(
-		'admin'		=> '',
-		'projects'	=> '',
-		'support'	=> '',
-		'reports'	=> '',
-	);
-	
-	//Set the current page as active.
-	$active[$this->current_system]='active';
-?>
-<div class="navbar-collapse collapse">
-    <ul class="nav navbar-nav">
-		<?php
-			if($this->session->userdata('employee_admin')=="CHECKED"){
-		?>
-		<li class="dropdown <?php echo $active['admin']; ?>">
-            <a href="#" class="dropdown-toggle" aria-expanded="false" role="button" data-toggle="dropdown">Admin <span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-                <li><?php echo anchor('admin/departments', 'Departments'); ?></li>
-                <li><?php echo anchor('admin/employees', 'Employees'); ?></li>
-                <li><?php echo anchor('admin/customers', 'Customers'); ?></li>
-            </ul>
-        </li>
-		<?php
-			}
-		?>
-		<li class="dropdown">
-            <a href="#" class="dropdown-toggle" aria-expanded="false" role="button" data-toggle="dropdown">New <span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-                <li><?php echo anchor('projects/form', 'New Project'); ?></li>
-                <li><?php echo anchor('support/form', 'New Support'); ?></li>
-            </ul>
-        </li>
-		<li class="<?php echo $active['projects']; ?>"><?php echo anchor('projects','Projects'); ?></li>
-		<li class="<?php echo $active['support']; ?>"><?php echo anchor('support','Support'); ?></li>
-		<li class="<?php echo $active['reports']; ?>"><?php echo anchor('reports','Reports'); ?></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-			<a href="#" class="dropdown-toggle" aria-expanded="false" role="button" data-toggle="dropdown"><?php echo $this->session->userdata('employee_name'); ?> <span class="caret"></span></a>
-			<ul class="dropdown-menu" role="menu">
-				<li><?php echo anchor('user/settings', 'Settings'); ?></li>
-				<li class="divider"></li>
-				<li><?php echo anchor('employee/logout', 'Sign Out'); ?></li>
-			</ul>
-		</li>
-    </ul>
-</div>
+<nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item <?php echo ($this->current_system == "" ? 'is-active' : '') ?>" href="<?php echo base_url('/'); ?>"><i class="fas fa-home"></i> Home</a>
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+            <a href="<?php echo base_url('projects'); ?>" class="navbar-item <?php echo ($this->current_system == "projects" ? 'is-active' : '') ?>"><i class="fas fa-project-diagram"></i> Project</a>
+            <a href="<?php echo base_url('support'); ?>" class="navbar-item <?php echo ($this->current_system == "support" ? 'is-active' : '') ?>"><i class="fas fa-bug"></i> Support</a>
+            <a href="<?php echo base_url('reports'); ?>" class="navbar-item <?php echo ($this->current_system == "reports" ? 'is-active' : '') ?>"><i class="fas fa-chart-pie"></i> Reports</a>
+        </div>
+        <div class="navbar-end">
+            <a href="<?php echo base_url('projects/form'); ?>" class="navbar-item <?php echo ($this->current_system == "projects" && $this->current_page == "form" ? 'is-active' : '') ?>"><i class="fas fa-plus-square"></i> New Project</a>
+            <a href="<?php echo base_url('support/form'); ?>" class="navbar-item <?php echo ($this->current_system == "support" && $this->current_page == "form" ? 'is-active' : '') ?>"><i class="fas fa-plus-square"></i> New Support</a>
+            <div class="navbar-item">
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link"><i class="fas fa-cogs"></i> Settings</a>
+                    <div class="navbar-dropdown">
+                        <a href="<?php echo base_url('user/settings'); ?>" class="navbar-item">Settings</a>
+                        <hr class="navbar-divider" />
+                        <a href="<?php echo base_url('employee/logout'); ?>" class="navbar-item">Log Out</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
--- application/views/template.php
+++ application/views/template.php
@@ -16,38 +16,7 @@
     </head>
     <body>
         <div id="header">
-            <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
-                <div class="navbar-brand">
-                    <a class="navbar-item <?php echo ($this->current_system == "" ? 'is-active' : '') ?>" href="<?php echo base_url('/'); ?>"><i class="fas fa-home"></i> Home</a>
-                    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a>
-                </div>
-
-                <div id="navbarBasicExample" class="navbar-menu">
-                    <div class="navbar-start">
-                        <a href="<?php echo base_url('projects'); ?>" class="navbar-item <?php echo ($this->current_system == "projects" ? 'is-active' : '') ?>"><i class="fas fa-project-diagram"></i> Project</a>
-                        <a href="<?php echo base_url('support'); ?>" class="navbar-item <?php echo ($this->current_system == "support" ? 'is-active' : '') ?>"><i class="fas fa-bug"></i> Support</a>
-                        <a href="<?php echo base_url('reports'); ?>" class="navbar-item <?php echo ($this->current_system == "reports" ? 'is-active' : '') ?>"><i class="fas fa-chart-pie"></i> Reports</a>
-                    </div>
-                    <div class="navbar-end">
-                        <a href="<?php echo base_url('projects/form'); ?>" class="navbar-item <?php echo ($this->current_system == "projects" && $this->current_page == "form" ? 'is-active' : '') ?>"><i class="fas fa-plus-square"></i> New Project</a>
-                        <a href="<?php echo base_url('support/form'); ?>" class="navbar-item <?php echo ($this->current_system == "support" && $this->current_page == "form" ? 'is-active' : '') ?>"><i class="fas fa-plus-square"></i> New Support</a>
-                        <div class="navbar-item">
-                            <div class="navbar-item has-dropdown is-hoverable">
-                                <a class="navbar-link"><i class="fas fa-cogs"></i> Settings</a>
-                                <div class="navbar-dropdown">
-                                    <a href="<?php echo base_url('user/settings'); ?>" class="navbar-item">Settings</a>
-                                    <hr class="navbar-divider" />
-                                    <a href="<?php echo base_url('employee/logout'); ?>" class="navbar-item">Log Out</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
+            <?php $this->load->view('navigation'); ?>
         </div>
         
         <div id="content">
