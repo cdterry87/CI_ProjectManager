21dea3addabfd0f1e64f7562d8b465f261103d0a
= <chase.terry87@gmail.com>
2019-01-27 21:07:15 -0600

    Slightly modified the look of the task form.

--- application/views/projects/projects_tasks_form.php
+++ application/views/projects/projects_tasks_form.php
@@ -8,95 +8,95 @@
     </div>
     <div class="modal-card-body">
         <div id="ajax-messages"></div>
-        <br>
         <div class="content">
-            <div class="columns">
+            <div class="columns is-multiline">
                 <div class="column is-half">
                     <?php echo form_label('Assign Date:', 'task_assigned_date_mo', 'class="label"'); ?>
                     <div class="field is-grouped">
-                        <p class="control">
+                        <div class="control">
                             <?php echo form_input('task_assigned_date_mo', date('m'), 'class="input is-small" maxlength="2" size="2" data-required data-month data-autotab data-label="Assigned Date Month"'); ?>
-                        </p>
-                        <p class="control slash">/</p>
-                        <p class="control">
+                        </div>
+                        <div class="control slash">/</div>
+                        <div class="control">
                             <?php echo form_input('task_assigned_date_day', date('d'), 'class="input is-small" maxlength="2" size="2" data-required data-day data-autotab data-label="Assigned Date Day"'); ?>
-                        </p>
-                        <p class="control slash">/</p>
-                        <p class="control">
+                        </div>
+                        <div class="control slash">/</div>
+                        <div class="control">
                             <?php echo form_input('task_assigned_date_yr', date('Y'), 'class="input is-small" maxlength="4" size="4" data-required data-year data-label="Assigned Date Year"'); ?>
-                        </p>
+                        </div>
                     </div>
                 </div>
                 <div class="column is-half">
                     <?php echo form_label('Start Date:', 'task_start_date_mo', 'class="label"'); ?>
                     <div class="field is-grouped">
-                        <p class="control">
+                        <div class="control">
                             <?php echo form_input('task_start_date_mo', '', 'class="input is-small" maxlength="2" size="2" data-required data-month data-autotab data-label="Start Date Month"'); ?>
-                        </p>
-                        <p class="control slash">/</p>
-                        <p class="control">
+                        </div>
+                        <div class="control slash">/</div>
+                        <div class="control">
                             <?php echo form_input('task_start_date_day', '', 'class="input is-small" maxlength="2" size="2" data-required data-day data-autotab data-label="Start Date Day"'); ?>
-                        </p>
-                        <p class="control slash">/</p>
-                        <p class="control">
+                        </div>
+                        <div class="control slash">/</div>
+                        <div class="control">
                             <?php echo form_input('task_start_date_yr', date('Y'), 'class="input is-small" maxlength="4" size="4" data-required data-year data-label="Start Date Year"'); ?>
-                        </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="columns">
                 <div class="column is-half">
                     <?php echo form_label('Due Date:', 'task_due_date_mo', 'class="label"'); ?>
                     <div class="field is-grouped">
-                        <p class="control">
+                        <div class="control">
                             <?php echo form_input('task_due_date_mo', '', 'class="input is-small" maxlength="2" size="2" data-month data-autotab data-label="Due Date Month"'); ?>
-                        </p>
-                        <p class="control slash">/</p>
-                        <p class="control">
+                        </div>
+                        <div class="control slash">/</div>
+                        <div class="control">
                             <?php echo form_input('task_due_date_day', '', 'class="input is-small" maxlength="2" size="2" data-day data-autotab data-label="Due Date Day"'); ?>
-                        </p>
-                        <p class="control slash">/</p>
-                        <p class="control">
+                        </div>
+                        <div class="control slash">/</div>
+                        <div class="control">
                             <?php echo form_input('task_due_date_yr', '', 'class="input is-small" maxlength="4" size="4" data-year data-label="Due Date Year"'); ?>
-                        </p>
+                        </div>
                     </div>
                 </div>
                 <div class="column is-half">
                     <?php echo form_label('Completed Date:', 'task_completed_date_mo', 'class="label"'); ?>
                     <div class="field is-grouped">
-                        <p class="control">
+                        <div class="control">
                             <?php echo form_input('task_completed_date_mo', '', 'class="input is-small" maxlength="2" size="2" data-month data-autotab data-label="Completed Date Month"'); ?>
-                        </p>
-                        <p class="control slash">/</p>
-                        <p class="control">
+                        </div>
+                        <div class="control slash">/</div>
+                        <div class="control">
                             <?php echo form_input('task_completed_date_day', '', 'class="input is-small" maxlength="2" size="2" data-day data-autotab data-label="Completed Date Day"'); ?>
-                        </p>
-                        <p class="control slash">/</p>
-                        <p class="control">
+                        </div>
+                        <div class="control slash">/</div>
+                        <div class="control">
                             <?php echo form_input('task_completed_date_yr', '', 'class="input is-small" maxlength="4" size="4" data-year data-label="Completed Date Year"'); ?>
-                        </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="column is-full">
+                    <div class="field">
+                        <div class="control">
+                            <?php echo form_label('Task Details:', 'task_title', 'class="label"'); ?>
+                            <?php echo form_input('task_title', '', 'class="input is-small" data-required placeholder="Enter task title" maxlength="100"'); ?>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <?php echo form_textarea('task_description', '', 'class="textarea is-small" placeholder="Enter task description" rows="3"'); ?>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="field">
+                        <div class="control">
+                            <?php echo form_label('Assigned Employee(s):', '', 'class="label"');  ?>
+                            <?php echo form_checkbox('all_employees'); ?>
+                            <?php echo form_label('All Employees', 'all_employees'); ?>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="field">
-                <div class="control">
-                    <?php echo form_label('Task Details:', 'task_title', 'class="label"'); ?>
-                    <?php echo form_input('task_title', '', 'class="input is-small" data-required placeholder="Enter task title" maxlength="100"'); ?>
-                </div>
-            </div>
-            <div class="field">
-                <div class="control">
-                    <?php echo form_textarea('task_description', '', 'class="textarea is-small" placeholder="Enter task description" rows="3"'); ?>
-                </div>
-            </div>
-            <hr>
-            <div class="field">
-                <div class="control">
-                    <?php echo form_label('Assigned Employee(s):', '', 'class="label"');  ?>
-                    <?php echo form_checkbox('all_employees'); ?>
-                    <?php echo form_label('All Employees', 'all_employees'); ?>
-                </div>
-            </div>
+            
 
             <div class="columns is-multiline is-gapless">
                 <?php
