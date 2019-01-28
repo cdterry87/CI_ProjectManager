05048d4ad8a3c2503a3a7f2d5eb23a30c404ef4e
= <chase.terry87@gmail.com>
2019-01-27 20:30:47 -0600

    Added a truncates file to the database layouts directory to empty the database.

---
+++ _database_layout/truncates.sql
@@ -0,0 +1,37 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf600
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 truncate table customers;\
+truncate table customers_contacts;\
+truncate table customers_files;\
+truncate table customers_notes;\
+truncate table customers_reminders;\
+truncate table customers_reminders_employees;\
+truncate table customers_systems;\
+truncate table departments;\
+truncate table departments_employees;\
+truncate table departments_projects;\
+truncate table departments_support;\
+truncate table employees_projects;\
+truncate table employees_reminders;\
+truncate table employees_support;\
+truncate table files_documentation;\
+truncate table files_forms;\
+truncate table projects;\
+truncate table projects_files;\
+truncate table projects_history;\
+truncate table projects_notes;\
+truncate table projects_reminders;\
+truncate table projects_reminders_employees;\
+truncate table projects_tasks;\
+truncate table projects_tasks_employees;\
+truncate table sessions;\
+truncate table support;\
+truncate table support_files;\
+truncate table support_history;\
+truncate table support_tasks;\
+truncate table systems;}
